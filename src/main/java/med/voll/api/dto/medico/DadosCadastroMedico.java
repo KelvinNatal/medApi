6fd46b23f4004173a8a93adee8bed002acbf5251
KelvinNatal <kelvinnatal@hotmail.com>
2023-05-13 20:21:05 -0300

    Entities e Repository

--- src/main/java/med/voll/api/dto/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/dto/medico/DadosCadastroMedico.java
@@ -1,6 +1,6 @@
-package med.voll.api.medico;
+package med.voll.api.dto.medico;
 
-import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.dto.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(String nome, String email, String crm, Especialidade especialidade, DadosEndereco endereco) {
 
